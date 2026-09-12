5836967054cb1b8a76401fbab32f34ab3d30ee0d
yuchi <52803366+insomniac-engineer@users.noreply.github.com>
2026-09-12 23:57:27 +0100

    Time: 15 ms (80.5%), Space: 19.4 MB (19.22%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,20 +1,34 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        pattern = Counter(s1)
-        seen = defaultdict(int)
+        if len(s1) > len(s2):
+            return False
+
+        s1Count, s2Count = [0] * 26, [0] * 26
+        for i in range(len(s1)):
+            s1Count[ord(s1[i]) - ord('a')] += 1
+            s2Count[ord(s2[i]) - ord('a')] += 1
+
+        matches = 0
+        for i in range(26):
+            matches += (1 if s1Count[i] == s2Count[i] else 0)
+
         l = 0
-        for r in range(len(s2)):
-            # [e] -> 1
-            seen[s2[r]] += 1
-
-            while (r - l + 1) > len(s1):
-                seen[s2[l]] -= 1
-
-                if seen[s2[l]] == 0:
-                    del seen[s2[l]]
-                l += 1
-            
-            if pattern == seen:
+        for r in range(len(s1), len(s2)):
+            if matches == 26:
                 return True
-        return False
-        
+
+            index = ord(s2[r]) - ord('a')
+            s2Count[index] += 1
+            if s1Count[index] == s2Count[index]:
+                matches += 1
+            elif s1Count[index] + 1 == s2Count[index]:
+                matches -= 1
+
+            index = ord(s2[l]) - ord('a')
+            s2Count[index] -= 1
+            if s1Count[index] == s2Count[index]:
+                matches += 1
+            elif s1Count[index] - 1 == s2Count[index]:
+                matches -= 1
+            l += 1
+        return matches == 26
