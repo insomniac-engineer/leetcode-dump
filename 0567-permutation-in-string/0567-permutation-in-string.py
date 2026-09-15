b152d68cbf279c16f3e480e66b4427e1a9ed6175
yuchi <52803366+insomniac-engineer@users.noreply.github.com>
2026-09-15 22:53:58 +0100

    Time: 13 ms (84.81%), Space: 19.4 MB (54.97%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -5,18 +5,18 @@ class Solution:
         # permutation -> the count must be equal, values can be shuffled (order matters)
         pattern = Counter(s1) # [a] -> 1 [b] -> 2
         l = 0
-        current_window = defaultdict(int)
+        seen = defaultdict(int)
         len_s1 = len(s1)
         for r in range(len(s2)):
-            current_window[s2[r]] += 1 # e -> 1
+            seen[s2[r]] += 1 # e -> 1
 
             if r - l + 1 > len_s1:
-                current_window[s2[l]] -= 1
-                if current_window[s2[l]] <= 0:
-                    del current_window[s2[l]]
+                seen[s2[l]] -= 1
+                if seen[s2[l]] <= 0:
+                    del seen[s2[l]]
                 l += 1
 
             # We are comparing n times 26 ASCII symbols (characters)
-            if current_window and current_window == pattern: return True
+            if seen == pattern: return True
             
         return False
