d1b4fedae02fab0a80adba22c5ab8cac9f66f8d3
yuchi <52803366+insomniac-engineer@users.noreply.github.com>
2026-09-16 22:51:09 +0100

    Time: 27 ms (39.86%), Space: 19.4 MB (19.27%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -6,15 +6,24 @@ class Solution:
         seen = defaultdict(int)
         len_s1 = len(s1)
         l = 0
+        need, have = len(pattern), 0
         for r in range(len(s2)):
             seen[s2[r]] += 1
-
+            # match happens only if we have enough frequency (from need) for each key
+            if pattern[s2[r]] == seen[s2[r]]:
+                have += 1
             while r - l + 1 > len_s1:
+                if pattern[s2[l]] == seen[s2[l]]:
+                    have -= 1
                 seen[s2[l]] -= 1
                 if seen[s2[l]] == 0:
                     del seen[s2[l]]
                 l += 1
-            if pattern == seen:
+            # # takes O(n * 26)
+            # if pattern == seen:
+            #     return True
+            # instead of comparing entire dict we compare counters
+            if have == need:
                 return True
         return False
 
