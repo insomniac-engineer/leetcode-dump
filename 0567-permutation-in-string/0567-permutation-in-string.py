3e5a459f5341e9f4140ed85b3ced44b9a2e23e90
yuchi <52803366+insomniac-engineer@users.noreply.github.com>
2026-09-19 00:00:28 +0100

    Time: 27 ms (39.68%), Space: 19.5 MB (19.24%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -6,16 +6,21 @@ class Solution:
 
         seen = defaultdict(int)
         l = 0
+        # Count +1 only when ALL the frequencies match
+        need, have = len(pattern), 0
         for r in range(len(s2)):
 
             seen[s2[r]] += 1
-
+            if seen[s2[r]] == pattern[s2[r]]:
+                have += 1
             while r - l + 1 > len(s1):
+                if seen[s2[l]] == pattern[s2[l]]:
+                    have -= 1
                 seen[s2[l]] -= 1
                 if seen[s2[l]] == 0:
                     del seen[s2[l]]
                 l += 1
             
-            if seen == pattern:
+            if have == need:
                 return True
         return False
