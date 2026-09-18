019c5e64bb4d9f54d7dc3194f8fb832cd7617658
yuchi <52803366+insomniac-engineer@users.noreply.github.com>
2026-09-18 23:51:21 +0100

    Time: 19 ms (59.38%), Space: 19.4 MB (19.24%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,30 +1,21 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        # permutation -> we care about chars frequency (dict)
+        # permutation - chars frequencies are the same
 
         pattern = Counter(s1)
+
         seen = defaultdict(int)
-        len_s1 = len(s1)
         l = 0
-        need, have = len(pattern), 0
         for r in range(len(s2)):
+
             seen[s2[r]] += 1
-            # match happens only if we have enough frequency (from need) for each key
-            if pattern[s2[r]] == seen[s2[r]]:
-                have += 1
-            while r - l + 1 > len_s1:
-                if pattern[s2[l]] == seen[s2[l]]:
-                    have -= 1
+
+            while r - l + 1 > len(s1):
                 seen[s2[l]] -= 1
                 if seen[s2[l]] == 0:
                     del seen[s2[l]]
                 l += 1
-            # # takes O(n * 26)
-            # if pattern == seen:
-            #     return True
-            # instead of comparing entire dict we compare counters
-            if have == need:
+            
+            if seen == pattern:
                 return True
         return False
-
-        
