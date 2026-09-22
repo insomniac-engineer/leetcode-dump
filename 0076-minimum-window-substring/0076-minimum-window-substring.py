32990f4f3fd7dfdb2582183b00f2ef44b4c71be6
yuchi <52803366+insomniac-engineer@users.noreply.github.com>
2026-09-22 22:56:53 +0100

    Time: 71 ms (53.4%), Space: 19.6 MB (74.5%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,12 +1,17 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
         # All the chars from t is present in result + extra
+        # TC: O(N)
+        # SC: O(N)
         if len(s) < len(t): return ""
+
         final_res = ""
         pattern = Counter(t)
         have, need = 0, len(pattern)
         seen = defaultdict(int)
         l = 0
+        best_r, best_l = len(s) - 1, 0
+        min_len = float("inf")
         for r in range(len(s)):
             seen[s[r]] += 1
 
@@ -14,15 +19,13 @@ class Solution:
                 have += 1
 
             while have == need:
-                if final_res == "":
-                    final_res = s[l:r+1]
-                else:         
-                    if len(final_res) >= len(s[l:r+1]):
-                        final_res = s[l:r+1]
+                min_len = r - l + 1
+                if min_len < best_r - best_l + 1:
+                    best_l, best_r = l, r
                 if s[l] in pattern and seen[s[l]] == pattern[s[l]]:
                     have -= 1
                 seen[s[l]] -= 1
                 if seen[s[l]] == 0:
                     del seen[s[l]]
                 l+= 1
-        return final_res
+        return s[best_l: best_r + 1] if min_len != float("inf") else ""
