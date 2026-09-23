a5a1a9c97f185505ff7cfb7e6f52e8e8ead8f862
yuchi <52803366+insomniac-engineer@users.noreply.github.com>
2026-09-23 22:52:01 +0100

    Time: 0 ms (100%), Space: 19.2 MB (74.21%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -10,18 +10,20 @@ class Solution:
 
         while list1 and list2:
             if list1.val <= list2.val:
-                dummy.next = list1
+                tail.next = list1
                 list1 = list1.next
             else:
-                dummy.next = list2
+                tail.next = list2
                 list2 = list2.next
-            dummy = dummy.next
-        while list1:
-            dummy.next = list1
-            list1 = list1.next
-            dummy = dummy.next
-        while list2:
-            dummy.next = list2
-            list2 = list2.next
-            dummy = dummy.next
-        return tail.next
+            tail = tail.next
+        # Elegant way to append the rest of list O(1)
+        tail.next = list1 or list2
+        # while list1:
+        #     dummy.next = list1
+        #     list1 = list1.next
+        #     dummy = dummy.next
+        # while list2:
+        #     dummy.next = list2
+        #     list2 = list2.next
+        #     dummy = dummy.next
+        return dummy.next
